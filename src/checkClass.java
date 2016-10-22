057eb49a8095a15c7b457ddc8e2d9283ba91f1af
crazyartus <crazyartus@gmail.com>
2016-10-23 01:04:52 +0200

    Update checkClass.java

--- src/checkClass.java
+++ src/checkClass.java
@@ -2,6 +2,7 @@
 public class checkClass {
     public static void main(String[] args){
         System.out.println("hello world");
+        System.out.println("hello from the other side");
 
     }
 }
